3c570a87363726ec2b2205648160a164ecc84f8b
Keyan Andy Delgado <96453690+2ez4Keyan@users.noreply.github.com>
2023-11-17 22:36:45 +0800

    updated sql

--- nena.sql
+++ nena.sql
@@ -1,20 +1,21 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.7.1
--- http://www.phpmyadmin.net
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 13, 2015 at 01:02 PM
--- Server version: 5.5.39
--- PHP Version: 5.4.31
+-- Generation Time: Nov 17, 2023 at 03:36 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `nena`
@@ -26,19 +27,21 @@ SET time_zone = "+00:00";
 -- Table structure for table `appointment`
 --
 
-CREATE TABLE IF NOT EXISTS `appointment` (
-`appId` int(3) NOT NULL,
-  `patientIc` bigint(12) NOT NULL,
+CREATE TABLE `appointment` (
+  `appId` int(3) NOT NULL,
+  `philhealthId` bigint(12) NOT NULL,
   `scheduleId` int(10) NOT NULL,
   `appSymptom` varchar(100) NOT NULL,
   `appComment` varchar(100) NOT NULL,
   `status` varchar(10) NOT NULL DEFAULT 'process'
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=87 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `appointment`
 --
 
+INSERT INTO `appointment` (`appId`, `philhealthId`, `scheduleId`, `appSymptom`, `appComment`, `status`) VALUES
+(90, 19283910923, 47, 'qwe', 'qwe', 'process');
 
 -- --------------------------------------------------------
 
@@ -46,7 +49,7 @@ CREATE TABLE IF NOT EXISTS `appointment` (
 -- Table structure for table `doctor`
 --
 
-CREATE TABLE IF NOT EXISTS `doctor` (
+CREATE TABLE `doctor` (
   `icDoctor` bigint(12) NOT NULL,
   `password` varchar(20) NOT NULL,
   `doctorId` int(3) NOT NULL,
@@ -56,7 +59,7 @@ CREATE TABLE IF NOT EXISTS `doctor` (
   `doctorPhone` varchar(15) NOT NULL,
   `doctorEmail` varchar(20) NOT NULL,
   `doctorDOB` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `doctor`
@@ -71,25 +74,24 @@ INSERT INTO `doctor` (`icDoctor`, `password`, `doctorId`, `doctorFirstName`, `do
 -- Table structure for table `doctorschedule`
 --
 
-CREATE TABLE IF NOT EXISTS `doctorschedule` (
-`scheduleId` int(11) NOT NULL,
+CREATE TABLE `doctorschedule` (
+  `scheduleId` int(11) NOT NULL,
   `scheduleDate` date NOT NULL,
   `scheduleDay` varchar(15) NOT NULL,
   `startTime` time NOT NULL,
   `endTime` time NOT NULL,
   `bookAvail` varchar(10) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=45 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `doctorschedule`
 --
 
 INSERT INTO `doctorschedule` (`scheduleId`, `scheduleDate`, `scheduleDay`, `startTime`, `endTime`, `bookAvail`) VALUES
-(40, '2015-12-13', 'Sunday', '09:00:00', '10:00:00', 'notavail'),
-(41, '2015-12-13', 'Sunday', '10:00:00', '11:00:00', 'available'),
 (42, '2015-12-13', 'Sunday', '11:00:00', '12:00:00', 'available'),
 (43, '2015-12-14', 'Monday', '11:00:00', '12:00:00', 'available'),
-(44, '2015-12-13', 'Sunday', '01:00:00', '02:00:00', 'available');
+(44, '2015-12-13', 'Sunday', '01:00:00', '02:00:00', 'available'),
+(47, '2023-11-17', 'Friday', '09:40:00', '01:00:00', 'notavail');
 
 -- --------------------------------------------------------
 
@@ -97,8 +99,8 @@ INSERT INTO `doctorschedule` (`scheduleId`, `scheduleDate`, `scheduleDay`, `star
 -- Table structure for table `patient`
 --
 
-CREATE TABLE IF NOT EXISTS `patient` (
-  `icPatient` bigint(12) NOT NULL,
+CREATE TABLE `patient` (
+  `philhealthId` bigint(12) NOT NULL,
   `password` varchar(20) NOT NULL,
   `patientFirstName` varchar(20) NOT NULL,
   `patientLastName` varchar(20) NOT NULL,
@@ -108,13 +110,15 @@ CREATE TABLE IF NOT EXISTS `patient` (
   `patientAddress` varchar(100) NOT NULL,
   `patientPhone` varchar(15) NOT NULL,
   `patientEmail` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `patient`
 --
 
-INSERT INTO `patient` (`icPatient`, `password`, `patientFirstName`, `patientLastName`, `patientMaritialStatus`, `patientDOB`, `patientGender`, `patientAddress`, `patientPhone`, `patientEmail`) VALUES
+INSERT INTO `patient` (`philhealthId`, `password`, `patientFirstName`, `patientLastName`, `patientMaritialStatus`, `patientDOB`, `patientGender`, `patientAddress`, `patientPhone`, `patientEmail`) VALUES
+(12345678910, '123', 'Test', 'Patient', '', '2002-05-20', 'male', '', '123123', 'test@gmail.com'),
+(19283910923, '123', 'Keyan', 'Delgado', 'single', '2002-05-22', 'male', 'Brgy. Ambalgan Poblacion Of Sto Nino South Cotabato', '9262408442', 'admin@gmail.com'),
 (920517105553, '123', 'Mohd', 'Mazlan', 'single', '1992-05-17', 'male', 'NO 153 BLOK MURNI\r\nKOLEJ CANSELOR UNIVERSITI PUTRA MALAYSIA', '173567758', 'lan.psis@gmail.com');
 
 --
@@ -125,25 +129,28 @@ INSERT INTO `patient` (`icPatient`, `password`, `patientFirstName`, `patientLast
 -- Indexes for table `appointment`
 --
 ALTER TABLE `appointment`
- ADD PRIMARY KEY (`appId`), ADD UNIQUE KEY `scheduleId_2` (`scheduleId`), ADD KEY `patientIc` (`patientIc`), ADD KEY `scheduleId` (`scheduleId`);
+  ADD PRIMARY KEY (`appId`),
+  ADD UNIQUE KEY `scheduleId_2` (`scheduleId`),
+  ADD KEY `patientIc` (`philhealthId`),
+  ADD KEY `scheduleId` (`scheduleId`);
 
 --
 -- Indexes for table `doctor`
 --
 ALTER TABLE `doctor`
- ADD PRIMARY KEY (`icDoctor`);
+  ADD PRIMARY KEY (`icDoctor`);
 
 --
 -- Indexes for table `doctorschedule`
 --
 ALTER TABLE `doctorschedule`
- ADD PRIMARY KEY (`scheduleId`);
+  ADD PRIMARY KEY (`scheduleId`);
 
 --
 -- Indexes for table `patient`
 --
 ALTER TABLE `patient`
- ADD PRIMARY KEY (`icPatient`);
+  ADD PRIMARY KEY (`philhealthId`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -153,12 +160,14 @@ ALTER TABLE `patient`
 -- AUTO_INCREMENT for table `appointment`
 --
 ALTER TABLE `appointment`
-MODIFY `appId` int(3) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=87;
+  MODIFY `appId` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=91;
+
 --
 -- AUTO_INCREMENT for table `doctorschedule`
 --
 ALTER TABLE `doctorschedule`
-MODIFY `scheduleId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=45;
+  MODIFY `scheduleId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+
 --
 -- Constraints for dumped tables
 --
@@ -167,8 +176,9 @@ MODIFY `scheduleId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=45;
 -- Constraints for table `appointment`
 --
 ALTER TABLE `appointment`
-ADD CONSTRAINT `appointment_ibfk_4` FOREIGN KEY (`patientIc`) REFERENCES `patient` (`icPatient`),
-ADD CONSTRAINT `appointment_ibfk_5` FOREIGN KEY (`scheduleId`) REFERENCES `doctorschedule` (`scheduleId`);
+  ADD CONSTRAINT `appointment_ibfk_4` FOREIGN KEY (`philhealthId`) REFERENCES `patient` (`philhealthId`),
+  ADD CONSTRAINT `appointment_ibfk_5` FOREIGN KEY (`scheduleId`) REFERENCES `doctorschedule` (`scheduleId`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
